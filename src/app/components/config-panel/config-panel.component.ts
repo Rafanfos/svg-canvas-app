986032fcd5ffe47bcdc459bec371811d8e490996
rafanfos <rafaelbsilva555@gmail.com>
2025-05-20 11:39:52 -0300

    feat: create components for canvas, config-panel and toolbar

---
+++ src/app/components/config-panel/config-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './config-panel.component.html',
+  styleUrl: './config-panel.component.scss'
+})
+export class ConfigPanelComponent {
+
+}
